42a8502230aecd5af1a3ddc645d06688ae89c0a8
epidermus <thelymanboy@gmail.com>
2025-04-18 11:52:43 -0600

    modified install script

--- installScript.sh
+++ installScript.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
-
-pacman -S --needed - < pkglist.txt
-
 WALLPAPER_DEST="~/Pictures/wallpapers/"
 
-#install eww
+echo "Installing neccessary packages"
+sudo pacman -S --needed - < pkglist.txt
+
+echo "Copying Hypyland Config"
 
 cp -r ./hyprConfig/* ~/.config/hypr/
 
-git clone https://github.com/elkowar/eww ./ewwRepo
+if ! command -v eww 2>&1 >/dev/null
+then
+  echo "Cloning Eww (Elkowar's Wacky Widgets) Repo"
 
-cd ewwRepo
+  git clone https://github.com/elkowar/eww ./ewwRepo
 
-cargo build --release --no-default-features --features=wayland
+  cd ewwRepo
 
-chmod +x ./target/release/eww
+  echo "Building Repo"
 
-cp ./target/release/eww /bin/eww
+  cargo build --release --no-default-features --features=wayland
 
-cd ..
+  chmod +x ./target/release/eww
 
-cp -r ./ewwConfig/* ~/.config/eww/
+  cp ./target/release/eww /bin/eww
+
+  cd ..
+
+fi
 
 
-cp -r ./alacrittyConfig/* ~/.config/alacritty/
+echo "Copying Eww Config"
 
-cp -r ./zellijConfig/* ~/.config/zellij/
+if [ ! -d ~/.config/eww ]; then mkdir ~/.config/eww ; fi && cp -r ./ewwConfig/* ~/.config/eww/
 
-mkdir -p $WALLPAPER_DEST && cp -r ./wallpapers/* $WALLPAPER_DEST 
+echo "Copying Alacritty Config"
+
+if [ ! -d ~/.config/alacritty ]; then mkdir ~/.config/alacritty ; fi && cp -r ./alacrittyConfig/* ~/.config/alacritty/
+
+echo "Copying Zellij Config"
+
+if [ ! -d ~/.config/zellij ]; then mkdir ~/.config/zellij ; fi && cp -r ./zellijConfig/* ~/.config/zellij/
+
+echo "Copying Wallpapers"
+
+ if [ ! -d $WALLPAPER_DEST ]; then mkdir -p $WALLPAPER_DEST ; fi && cp -r ./wallpapers/* $WALLPAPER_DEST 
 
 # copy hypr config to the hypr folder
 # install eww
